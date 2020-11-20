a2690306fac2583087fa66ffee8ccda3dba188c0
ionwolf13 <voli.scorp13@gmail.com>
2020-11-19 23:10:52 -0600

    commit tree a2582e51de010edb94db1e0b18cc81697b43734b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+        if self.end_with?(".") == true
+          return true
+        else
+          return false
+        end
   end
 
   def question?
-
+        if self.end_with?("?") == true
+          return true
+        else
+          return false
+        end
   end
 
   def exclamation?
-
+        if self.end_with?("!") == true
+          return true
+        else
+          return false
+        end
   end
 
   def count_sentences
-
+      count = 0
+      a= self.split
+         a.each do |string|
+            if
+            string.end_with?(".","?","!") == true
+            count += 1
+            end
+        end
+      count
   end
+
 end
